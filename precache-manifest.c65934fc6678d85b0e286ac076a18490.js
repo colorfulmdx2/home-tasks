c982f1ffccbb9b6f928917dd3dea225dfcbd24bd
colorfulmdx2 <colorfulmdx2@gmail.com>
2020-07-27 16:54:48 +0300

    Updates

---
+++ precache-manifest.c65934fc6678d85b0e286ac076a18490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569b014589dc30a4304fbae3c11bc242",
+    "url": "/home-tasks/index.html"
+  },
+  {
+    "revision": "fee29b4720f9a780e782",
+    "url": "/home-tasks/static/css/main.a7a85be8.chunk.css"
+  },
+  {
+    "revision": "171c270028cc35b985d1",
+    "url": "/home-tasks/static/js/2.0f4b5c81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/home-tasks/static/js/2.0f4b5c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee29b4720f9a780e782",
+    "url": "/home-tasks/static/js/main.1661270a.chunk.js"
+  },
+  {
+    "revision": "677d7e64c8c3cf631909",
+    "url": "/home-tasks/static/js/runtime-main.054bbdac.js"
+  },
+  {
+    "revision": "9f27236c94f9d731f593ef133ae7d4e7",
+    "url": "/home-tasks/static/media/background.9f27236c.jpg"
+  }
+]);
